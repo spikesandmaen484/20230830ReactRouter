e046dcbe46bf5a66bb5bae2f233e4efc94f40b05
Aden <cc31228@gmail.com>
2023-08-30 16:00:56 +0800

    git init

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import './App.css'
+import { HashRouter, NavLink, Outlet, Route, Routes, useNavigate, useParams  } from 'react-router-dom'
+
+const Post = () => {
+  return(<div>
+    <h3>Post 頁面</h3>
+    <Outlet />
+  </div>)
+}
+
+const PostId = () => {
+  let params = useParams();
+
+  return(<p>
+    Post ID: {params.post123}
+  </p>)
+}
+
+
+const LogOut = () => {
+  const navigate = useNavigate();
+  return(
+    <button onClick={() => {
+      navigate('/login');
+    }}>登出</button>
+  );
+}
+
+const Todo = () => {
+  return (
+    <>
+      <p>這是 Todo 頁面</p>
+      <LogOut />
+    </>
+    
+  );
+};
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+function App() {
+
+  return (
+    <div className="container">
+      <HashRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+          <NavLink to="/post/post123">
+            <p>Post 詳細頁面</p>
+          </NavLink>
+        </div>
+        {/* Routes, Route 練習區 */}
+        {/* 練習區 */}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":post123" element={<PostId />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </div>
+  )
+}
+
+export default App;
